7288c0b6f1d662b6953a958f17397757b9eb0da6
luda412 <rudaroy@naver.com>
2026-01-27 15:08:30 +0900

    [Feat] LangChain practice

---
+++ LangChain/poet/main.py
@@ -0,0 +1,22 @@
+from langchain.chat_models import init_chat_model
+from langchain_core.prompts import ChatPromptTemplate
+from langchain_core.output_parsers import StrOutputParser
+from dotenv import load_dotenv
+load_dotenv()
+
+#ChatOpenAI 초기화
+llm = init_chat_model("gpt-5-mini", model_provider="openai")
+
+#프롬프트 템플릿 생성
+prompt = ChatPromptTemplate.from_messages([
+    ("system", "You are a helpful assistant."),
+    ("user", "{input}")
+])
+
+#문자열 출력 파서
+output_parser = StrOutputParser()
+
+#LLM 체인 구성
+chain = prompt | llm | output_parser
+result = chain.invoke({"input": "hi, what is your name?"})
+print(result)
